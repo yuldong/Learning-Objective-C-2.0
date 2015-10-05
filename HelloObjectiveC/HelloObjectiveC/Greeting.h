db1427930cb3644ea0971af6832fb880387bacc5
yrion <yuldong@126.com>
2015-10-05 23:06:51 +0800

    调整文件结构 完成第四章的示例代码以及练习

---
+++ HelloObjectiveC/HelloObjectiveC/Greeting.h
@@ -0,0 +1,13 @@
+//
+//  Greeting.h
+//  HelloObjectiveC
+//
+//  Created by YrionPlus on 15/10/5.
+//  Copyright © 2015年 YrionPlus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Greeting : NSObject
+
+@end
